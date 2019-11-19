0dcae556b880a8ddb90f44bf868538fe255b83ef
kraftwerk28 <kefirchik3@gmail.com>
2019-11-19 12:34:41 +0200

    add: ssh pushing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
+const PORT = process.env.PORT || 8080
+
 http
   .createServer((_, res) => {
     res.writeHead(200, { 'content-type': 'text/plain' }).end('ok')
   })
-  .listen(process.env.PORT || 8080, () => console.log('Server listening...'))
+  .listen(PORT, () => console.log('Server listening on :' + PORT))
